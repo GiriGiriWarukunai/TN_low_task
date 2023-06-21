9081125e01ec3274f77c1113b87c3c5c42bd7c04
GiriGiriWarukunai <sasha-kurbanov@list.ru>
2023-06-21 11:32:06 +0300

    Task_3 new class

--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,45 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+class DeleteDuplicate {
+
+    int[] deleteDuplicate(int[] a){
+        boolean f = true;
+        int i;
+        //проходим все начальные элементы, которые не повторяются
+        for (i = 1; f && i < a.length; i++) {
+            for (int j = 0; f && j < i; j++) {
+                f = a[i] != a[j];
+            }
+        }
+        //если дошли до конца массива, значит повторяющихся элементов в массиве не было
+        if(f) {
+            return a;
+        }
+        else {
+            //я решил делать все в одном массиве, а не создавать новый, куда уже бы записывал уникальные элементы,
+            //поэтому w - индекс, на место которого будет записан следующий уникальный элемент
+            int w = i - 1;
+
+            for(int k = i; k < a.length; k++){
+                f = true;
+                for(int j = 0; f && j < k; j++)
+                    f = a[k] != a[j];
+                //если элемент a[k] раньше не встречался, то запишем его на место a[write]
+                if(f) {
+                    a[w] = a[k];
+                    w++;
+                }
+            }
+            //обрезаю оставшуюся часть массива
+            a = Arrays.copyOfRange(a, 0, w);
+            return a;
+
+        }
+
+    }
+}
+
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -8,42 +47,13 @@ public class Task2 {
         int n = scanner.nextInt();
         int[] a = new int[n];
         System.out.print("Введите массив чисел: ");
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
-
-
-        boolean f = true;
-        int i;
-        //проходим все начальные элементы, которые не повторяются
-        for(i = 1; f && i < n; i++)
-            for(int j = 0; f && j < i; j++)
-                f = a[i] != a[j];
-
-        //если дошли до конца массива, значит повторяющихся элементов в массиве не было
-        if(f)
-            System.out.println("Повторов в массиве не было");
-        else{
-            //я решил делать все в одном массиве, а не создавать новый, куда уже бы записывал уникальные элементы,
-            //поэтому write - индекс, на место которого будет записан следующий уникальный элемент
-            int write = i - 1;
-
-            for(int k = i; k < n; k++){
-                f = true;
-                for(int j = 0; f && j < k; j++)
-                    f = a[k] != a[j];
-                //если элемент a[k] раньше не встречался, то запишем его на место a[write]
-                if(f) {
-                    a[write] = a[k];
-                    write++;
-                }
-            }
-            //обрезаю оставшуюся часть массива
-            a = Arrays.copyOfRange(a, 0, write);
-            System.out.println("Исправленный массив: " + Arrays.toString(a));
-
         }
 
+        DeleteDuplicate deleteDuplicate = new DeleteDuplicate();
+        a = deleteDuplicate.deleteDuplicate(a);
 
-
+        System.out.println("Исправленный массив: " + Arrays.toString(a));
     }
 }
