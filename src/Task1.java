2186fe538efcecb924e466360a96602cdff32162
GiriGiriWarukunai <sasha-kurbanov@list.ru>
2023-06-21 11:19:48 +0300

    Task_1 new class

--- src/Task1.java
+++ src/Task1.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
+class Palindrome {
+    boolean isPalindrome (String s){
+        int i, j = s.length() - 1;
+        for (i = 0; i < s.length() / 2 && s.charAt(i) == s.charAt(j); i++, j--);
+        return i >= s.length() / 2;
+    }
+}
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите строку: ");
         String s = scanner.nextLine();
-        boolean res = true;
-        int j = s.length() - 1;
-        for(int i = 0; i < s.length() / 2 && res; i++, j--)
-            res = s.charAt(i) == s.charAt(j);
-        if(res)
-            System.out.println(s + " - палиндром");
-        else
-            System.out.println(s + " - не палиндром");
 
+        Palindrome palindrome = new Palindrome();
+        if (palindrome.isPalindrome(s)) {
+            System.out.println(s + " - палиндром");
+        }
+        else {
+            System.out.println(s + " - не палиндром");
+        }
     }
 }
