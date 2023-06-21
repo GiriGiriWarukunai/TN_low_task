5221a6499a9bffddff85c088463931f21cb5ca83
GiriGiriWarukunai <sasha-kurbanov@list.ru>
2023-06-21 11:24:28 +0300

    Task_3 new class

--- src/Task3.java
+++ src/Task3.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
+class Reverse {
+
+    String reverseString(String s){
+        int j = s.length() - 1;
+        //вариант решения через массив символов, без использования специальных методов для класса String
+        char[] a = s.toCharArray();
+        for (int i = 0; i < s.length() / 2; i++, j--){
+            char tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+        return new String(a);
+    }
+}
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите строку: ");
         String s = scanner.nextLine();
-        int j = s.length() - 1;
-        //вариант решения через массив символов, без использования специальных методов для класса String
-        char[] tmpstr = s.toCharArray();
-        for (int i = 0; i < s.length() / 2; i++, j--){
-            char tmp = tmpstr[i];
-            tmpstr[i] = tmpstr[j];
-            tmpstr[j] = tmp;
-        }
-        s = new String(tmpstr);
+
+        Reverse reverse = new Reverse();
+        s = reverse.reverseString(s);
         System.out.println("Новая строка: " + s);
     }
 }
