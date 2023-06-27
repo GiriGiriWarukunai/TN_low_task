eaf74431d56ad16decbb211eb7eece7ba7563771
GiriGiriWarukunai <sasha-kurbanov@list.ru>
2023-06-27 21:52:31 +0300

    Task_1 new class file

--- src/Task1.java
+++ src/Task1.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-class Palindrome {
-    boolean isPalindrome (String s){
-        int i, j = s.length() - 1;
-        for (i = 0; i < s.length() / 2 && s.charAt(i) == s.charAt(j); i++, j--);
-        return i >= s.length() / 2;
-    }
-}
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
