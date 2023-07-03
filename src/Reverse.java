0f0312642bf87b99fe52c4ae1663db58d55f325b
GiriGiriWarukunai <sasha-kurbanov@list.ru>
2023-07-03 22:33:31 +0300

    Task_3: class Swap added. Its include swapArrElems method

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,12 +1,11 @@
 public class Reverse {
 
     String reverseString(String s) {
+        Swap swap = new Swap();
         int j = s.length() - 1;
         char[] a = s.toCharArray();
         for (int i = 0; i < s.length() / 2; i++, j--) {
-            char tmp = a[i];
-            a[i] = a[j];
-            a[j] = tmp;
+            swap.swapArrElems(a, i, j);
         }
         return new String(a);
     }
